e6c4ea025c5fdd69b3b82bd2270a77515aed3cfb
gadore <qianlizhihao@gmail.com>
2017-09-10 19:39:24 +0800

    登录界面bug修复，access_token解决

--- src/main.js
+++ src/main.js
@@ -17,8 +17,7 @@ Vue.use(VueResource)
 // 每次请求都将检查登录状态
 router.beforeEach(({meta, path}, from, next) => {
   var { auth = true } = meta
-  var isLogin = Boolean(store.state.user.id) // true用户已登录， false用户未登录
-
+  var isLogin = Boolean(store.state.user.expires_in) // true用户已登录， false用户未登录
   if (auth && !isLogin && path !== '/login') {
     return next({ path: '/login' })
   }
--- src/store/user.js
+++ src/store/user.js
@@ -5,8 +5,31 @@ export const USER_SIGNIN = 'USER_SIGNIN' // 登录成功
 export const USER_SIGNOUT = 'USER_SIGNOUT' // 退出登录
 let loginApi = Api.login
 
+let accessSessionStorage = items => {
+  let states = {}
+  // for (let item in items) {
+  //   states.item = JSON.parse(sessionStorage.getItem(item)) || {}
+  // }
+  items.forEach(item => {
+    states[item] = JSON.parse(sessionStorage.getItem(item)) || {}
+  })
+  return states
+}
+
+let setSessionStorage = items => {
+  for (let item in items) {
+    sessionStorage.setItem(item, JSON.stringify(items[item]))
+  }
+}
+
 export default {
-  state: JSON.parse(sessionStorage.getItem('user')) || {},
+  // state: JSON.parse(sessionStorage.getItem('user')) || {},
+  state: accessSessionStorage([
+    'access_token',
+    'expires_in',
+    'token_type',
+    'userName'
+  ]),
   // {
   //   domain: 'http://test.example.com',
   //   userInfo: {
@@ -18,10 +41,15 @@ export default {
   // TODO: 添加用户登录，获取access_token
   mutations: {
     [USER_SIGNIN] (state, user) {
-      this.$http.post(loginApi.url, loginApi.request).then(success => {
-        sessionStorage.setItem('user', JSON.stringify(user))
+      this._devtoolHook.Vue.http.post(loginApi.url, loginApi.request).then(success => {
+        // sessionStorage.setItem('user', JSON.stringify(user))
+        setSessionStorage(success.data)
         Object.assign(state, user)
+        console.info(state)
+      }, failed => {
+        state['expires_in'] = 0
       })
+      console.info('login: ' + state)
     },
     [USER_SIGNOUT] (state) {
       sessionStorage.removeItem('user')
